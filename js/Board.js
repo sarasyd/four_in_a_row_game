8d3d4fdbefc6e670abe3cc5770c71c2a33fa712a
Sara Chen <sarachen@outlook.com>
2021-12-09 14:41:33 +1100

    Final complete project

--- js/Board.js
+++ js/Board.js
@@ -4,33 +4,35 @@ class Board {
         this.columns = 7;
         this.spaces = this.createSpaces();
     }
-
-    /*
-     * Create 2D array of speces
-     * return {array} an array of space objects
-    */
+    
+    /** 
+     * Generates 2D array of spaces. 
+     * @return  {array}     An array of space objects
+     */
     createSpaces() {
         const spaces = [];
-
-        for (let x = 0; x < this.columns; x++){
-            const column = [];
-
-            for (let y = 0; y < this.rows; y++){
-                const space = new Space(x, y);
-                column.push(space);
-            }
-            spaces.push(column);
-        }
-
+        
+		for (let x = 0; x < this.columns; x++) {
+			const col = [];
+			
+			for (let y = 0; y < this.rows; y++) {
+				const space = new Space(x, y);
+				col.push(space);
+			}
+			
+			spaces.push(col);
+		}
+        
         return spaces;
-
     }
-
-    //Render the Board
-    drawHTMLBoard() {
+    
+    /** 
+     * Draws associated SVG spaces for all game spaces.
+     */
+	drawHTMLBoard() {
         for (let column of this.spaces) {
             for (let space of column) {
-                space.drawSVGSpaces();
+                space.drawSVGSpace();
             }
         }
 	} 
--- js/Space.js
+++ js/Space.js
@@ -7,9 +7,23 @@ class Space {
         this.diameter = 76;
         this.radius = this.diameter/2;
     }
-
-    //Rendering Spaces
-    drawSVGSpaces() {
+	
+	/**
+	 * Checks if space has an associated token to find its owner
+	 * @return  {(null|Object)} Returns null or the owner object of the space's associated token.
+	 */
+	get owner() {
+        if (this.token === null) {
+            return null;
+        } else {
+            return this.token.owner;
+        }
+	}
+    
+    /**
+     * Draws SVG space
+     */
+	drawSVGSpace() {    
         const svgSpace = document.createElementNS("http://www.w3.org/2000/svg", "circle");
         svgSpace.setAttributeNS(null, "id", this.id);
         svgSpace.setAttributeNS(null, "cx", (this.x * this.diameter) + this.radius);
@@ -17,6 +31,16 @@ class Space {
         svgSpace.setAttributeNS(null, "r", this.radius - 8);
         svgSpace.setAttributeNS(null, "fill", "black");
         svgSpace.setAttributeNS(null, "stroke", "none");
-        document.getElementById("mask").appendChild(svgSpace);
+
+        document.getElementById("mask").appendChild(svgSpace);     
     }
+    
+    
+    /**
+     * Updates space to reflect a token has been dropped into it.
+     * @param {Object} token - The dropped token
+     */
+	mark(token) {
+		this.token = token;
+	}
 }
